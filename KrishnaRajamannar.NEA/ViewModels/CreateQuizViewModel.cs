689aad806b9e35813c8987e7e3f44b7c56f668c6
Krishna R <rajamannar@hotmail.com>
2024-01-06 21:11:27 +0000

    sorting out createquiz ui and cleaning it up

--- KrishnaRajamannar.NEA/ViewModels/CreateQuizViewModel.cs
+++ KrishnaRajamannar.NEA/ViewModels/CreateQuizViewModel.cs
@@ -1,12 +1,5 @@
-﻿using KrishnaRajamannar.NEA.Services.Database;
-using Microsoft.Identity.Client;
-using System;
-using System.Collections.Generic;
+﻿using KrishnaRajamannar.NEA.Services;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 
 namespace KrishnaRajamannar.NEA.ViewModels
 {
@@ -14,29 +7,21 @@ namespace KrishnaRajamannar.NEA.ViewModels
     {
 
         // This instantiates the class which handles the data being sent and recieved from the database for the Quiz table.
-        public QuizService _quizService = new QuizService();
-
+        //public QuizService _quizService = new QuizService();
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private readonly IQuizService _quizService;
+        // Used to identify which user is creating a quiz
+        // Data is passed on from the ViewQuizzes window
         public int UserID;
 
 
-        public CreateQuizViewModel()
+        public CreateQuizViewModel(IQuizService quizService)
         {
-            _quizService = new QuizService();
-        }
-
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        public void RaisePropertyChange(string propertyname)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
-            }
+            _quizService = quizService;
         }
 
         // This code is used for data binding with the UI. 
-
+        // Retrieves what the user inputs as the quiz title
         private string _quizTitle;
         public string QuizTitle 
         {
@@ -48,19 +33,47 @@ namespace KrishnaRajamannar.NEA.ViewModels
             }
         }
 
-        // This function takes in the userID as a parameter. 
+        // Binds with the UI
+        // Used to notify users of any errors with the quiz title input
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                RaisePropertyChange("Message");
+            }
+        }
+
+        // Used to bind with the UI
+        // Used to notify when the property's value changes
+        public void RaisePropertyChange(string propertyname)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+
+
+        // This procedure takes in the userID as a parameter. 
         // When creating a quiz, if no title has been inputted, an error message is displayed.
         // Otherwise, the method in QuizService is called which takes in the userID and QuizTitle to insert the quiz in the database. 
-        public void CreateQuiz() 
+        public bool CreateQuiz() 
         {
+            // Checks if quiz title inout is not empty 
             if ((QuizTitle != null) || (QuizTitle == ""))
             {
+                // Calls the service to create and store the quiz in the DB
                 _quizService.CreateQuiz(UserID, QuizTitle);
-                MessageBox.Show("Quiz created.");
+                Message = "Quiz created.";
+                return true;
             }
             else  
             {
-                MessageBox.Show("Invalid Input. Try again.");
+               Message = "Invalid Input. Try again.";
+                return false;
             }
         }
     }
